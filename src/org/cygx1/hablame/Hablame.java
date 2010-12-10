7e825ab97a62831fbde73d7b99e14cde8c2f541c
Julian Cerruti <jcerruti@ar.ibm.com>
2010-12-09 23:58:48 -0300

    Initial commit. Empty project

---
+++ src/org/cygx1/hablame/Hablame.java
@@ -0,0 +1,13 @@
+package org.cygx1.hablame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hablame extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
